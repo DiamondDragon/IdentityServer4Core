e868e6d6e9fad51a431246f093966724f736d3c7
Andei Salanoi <a.salanoi@godeltech.com>
2018-11-08 16:41:47 +0300

    Core client added

--- IdentityServer4PoC.ClientApp/Program.cs
+++ IdentityServer4PoC.ClientApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading.Tasks;
+using IdentityModel.Client;
 
 namespace IdentityServer4PoC.ClientApp
 {
@@ -6,7 +8,30 @@ namespace IdentityServer4PoC.ClientApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DoDemoAsync().Wait();
+
+        }
+
+        private static async Task DoDemoAsync()
+        {
+            var disco = await DiscoveryClient.GetAsync("http://localhost:5000");
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                return;
+            }
+
+
+            var tokenClient = new TokenClient(disco.TokenEndpoint, "client", "secret");
+            var tokenResponse = await tokenClient.RequestClientCredentialsAsync("api1");
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
         }
     }
 }
