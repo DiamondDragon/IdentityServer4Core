d340dfc6a8d5db55880933769e2918601376e1ef
Andei Salanoi <a.salanoi@godeltech.com>
2018-11-08 16:17:10 +0300

    Test applications are being added

---
+++ IdentityServer4PoC.ClientApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IdentityServer4PoC.ClientApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
